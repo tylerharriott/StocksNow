b0840217d6a53166ee7022360bd351fd5651e5c7
Tyler <tharriott02@gmail.com>
2019-03-26 12:07:22 -0400

    Added code for when the exit (X) button is clicked. Now it kills all threads alive.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class Driver extends Application {
@@ -16,11 +19,21 @@ public class Driver extends Application {
         primaryStage.show();
 
 
+        //Sometime cause slow down....  Once the user clicks (X), all threads will be killed
+        primaryStage.setOnHiding(event -> Platform.runLater(() -> {
+            System.out.println("Application Closed by click to Close Button(X)");
+            System.exit(0);
+        }));
+
+
+
     }
 
     public static void main(String[] args){
         launch(args);
 
     }
+
+
 
 }
