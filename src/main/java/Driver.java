b48036087f33782a9089bec8bab8debe5a71d6da
Tyler <tharriott02@gmail.com>
2019-03-27 11:28:15 -0400

    Added so size CANNOT be resizeable

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,8 +2,10 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -16,6 +18,7 @@ public class Driver extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Stocks Now");
         primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
 
 
